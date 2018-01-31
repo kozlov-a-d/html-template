13d97b5eda7f5989c6266414a7f9a2f4d45ab099
Andrey Kozlov <andreydmitrievich666@gmail.com>
2018-01-31 10:17:49 +0300

    footer bg fix

--- assets/ui-kit/core.js
+++ assets/ui-kit/core.js
@@ -15,6 +15,7 @@ app.scroll = scrollComponent;
 // app.scroll.direction() - return 'down'|'up'
 // app.scroll.disable() - disable scroll
 // app.scroll.enable() - enable scroll;
+// - app.scroll.speed() - return scroll speed value
 
 app.goal = goalComponent;
 // app.goal.reach(goalName, event) - trigger reach goal  ( event="onload" for window.load )
