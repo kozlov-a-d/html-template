7aeec95faaf702672d383e84d06634715c5f980c
Andrey Kozlov <andreydmitrievich666@gmail.com>
2018-06-29 14:28:03 +0300

    config

--- gulp/tasks/generate.js
+++ gulp/tasks/generate.js
@@ -2,31 +2,28 @@ const fs = require('fs');
 
 module.exports = (gulp) => {
 
-    const config = JSON.parse(fs.readFileSync('./frontend.config.json'));
-    const slots = config.slot;
-    const options = config.options;
+    let slots = JSON.parse(fs.readFileSync('./frontend.config.json')).slot;
+    let options = JSON.parse(fs.readFileSync('./templates-html/themes.json'));
 
-    let css = [''],
-        js = [''];
+    let css = [],
+        js = [],
+        html = [];
+    css.concat(2);
     for( let i = 0; i < slots.length; i++){
         let slotName = slots[i].name;
         let value = slots[i].value;
         for( let j = 0; j < options.length; j++ ){
-            if ( slotName  === options[j].slot &&
-                 value  === options[j].name){
-                // console.log(value);
-                console.log(options[j].parts.css);
-                console.log(options[j].parts.js);
-                css.concat(options[j].parts.css);
-                js.concat(options[j].parts.js);
-                console.log(css);
+            if ( slotName  === options[j].slot && value  === options[j].name){
+                css = css.concat(options[j].parts.css);
+                js = js.concat(options[j].parts.js);
+                // html = html.concat(options[j].parts.html);
             }
         }
     }
-    // console.log('------');
-    // console.log(css);
-    // console.log('------');
-    // console.log(js);
-    // console.log('------');
+    console.log('------');
+    console.log(css);
+    console.log('------');
+    console.log(js);
+    console.log('------');
 
 };
